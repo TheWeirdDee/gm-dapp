364abd905d5c5f4a3e503d53779ccae31c382f7e
TheWeirdDee <divinenation1@gmail.com>
2026-04-08 16:35:10 +0100

    Fix: Hardened address extraction in AuthHydrator to prevent runtime errors

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,38 @@
 'use client';
 
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { store } from '../lib/store';
+import { useEffect } from 'react';
+import { userSession, getUserData } from '../lib/stacks';
+import { setUserData } from '../lib/features/userSlice';
+
+function AuthHydrator({ children }: { children: React.ReactNode }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (userSession.isUserSignedIn()) {
+      const userData = userSession.loadUserData();
+      const stxAddress = userData.profile.stxAddress;
+      const addressString = typeof stxAddress === 'string' 
+        ? stxAddress 
+        : (stxAddress?.mainnet || stxAddress?.testnet || userData.authResponseToken);
+
+      dispatch(setUserData({
+        address: addressString,
+        profile: userData.profile
+      }));
+    }
+  }, [dispatch]);
+
+  return <>{children}</>;
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <AuthHydrator>
+        {children}
+      </AuthHydrator>
+    </Provider>
+  );
 }
