1372b63e4550a54826972f140212d579228ba44f
TheWeirdDee <divinenation1@gmail.com>
2026-04-15 21:32:41 +0100

    chore: update contract address and name config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -8,7 +8,7 @@ const IS_MAINNET = networkType === 'mainnet';
 export const APP_CONFIG = {
   // Use environment variables or fallback to the current verified Testnet deployment
   contractAddress: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || 'ST1MQE0HMB765Z9EVF0CM6SPMMKW4VPDDSVRTT2RF',
-  contractName: process.env.NEXT_PUBLIC_CONTRACT_NAME || 'gm-social-v2',
+  contractName: process.env.NEXT_PUBLIC_CONTRACT_NAME || 'gm-social-v5',
   
   // Set Network Object
   network: IS_MAINNET ? STACKS_MAINNET : STACKS_TESTNET,
